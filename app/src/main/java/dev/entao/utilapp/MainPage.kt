51a4a619ee77701b5406ce1b6c42b61fd8b37174
yangentao <entaoyang@163.com>
2019-04-23 13:37:44 +0800

    2.0.1  zxing3.3.3

--- app/src/main/java/dev/entao/utilapp/MainPage.kt
+++ app/src/main/java/dev/entao/utilapp/MainPage.kt
@@ -1,16 +1,21 @@
 package dev.entao.utilapp
 
 import android.content.Context
+import android.widget.EditText
 import android.widget.LinearLayout
+import dev.entao.appbase.App
 import dev.entao.log.logd
 import dev.entao.qr.QRPage
 import dev.entao.ui.base.openActivity
 import dev.entao.ui.base.pushPage
-import dev.entao.ui.creator.textView
+import dev.entao.ui.creator.buttonGreenRound
+import dev.entao.ui.creator.edit
 import dev.entao.ui.ext.*
 import dev.entao.ui.page.TitlePage
 
 class MainPage : TitlePage() {
+
+    lateinit var edit: EditText
 
     override fun onCreateContent(context: Context, contentView: LinearLayout) {
         super.onCreateContent(context, contentView)
@@ -20,6 +25,7 @@ class MainPage : TitlePage() {
                 val p = QRPage()
                 p.onScanText = {
                     logd("Scan: $it ")
+                    edit.textS = it
                 }
                 pushPage(p)
             }
@@ -28,11 +34,23 @@ class MainPage : TitlePage() {
             }
         }
 
-        contentView.textView(LParam.WidthFill.height(300).marginY(50)) {
-            text = "Main"
-            textColorRed()
-            gravityCenter()
+        edit = contentView.edit(LParam.WidthFill.height(120).margins(20)) {
+            gravityTopLeft()
+            setSingleLine(false)
+            isVerticalScrollBarEnabled = true
+            isHorizontalScrollBarEnabled = false
         }
+        contentView.buttonGreenRound {
+            text = "Clear"
+            onClick {
+                edit.textS = ""
+            }
+        }
+        contentView.buttonGreenRound {
+            text = "Copy"
+            App.copyToClipboard(edit.textS)
+        }
+
     }
 
 
